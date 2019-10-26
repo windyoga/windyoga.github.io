540f4fa2630fd5006ca8f141d2dbc6d288cd3997
cowcowlin2 <cowcowlin2@gmail.com>
2019-10-26 21:38:39 +0800

    20191026

--- javascript/teach_leftmenu.js
+++ javascript/teach_leftmenu.js
@@ -1,6 +1,6 @@
 document.write("<table width='100%' border='1' cellpadding='4' cellspacing='1' bordercolor='#BBCBA5'>");
 document.write("      <tr>");
-if (varleft!=7) {
+if (varleft!=0) {
 	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher.htm'>華云老師</a></td>");
 } else {
 	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher.htm'><font color='#ffffff'>華云老師</font></a></td>");
@@ -8,9 +8,9 @@ if (varleft!=7) {
 document.write("      </tr>");
 document.write("      <tr>");
 if (varleft!=1) {
-	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher7.htm'>英玲老師</a></td>");
+	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher7_shi.htm'>英玲老師</a></td>");
 } else {
-	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher7.htm'><font color='#ffffff'>英玲老師</font></a></td>");
+	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher7_shi.htm'><font color='#ffffff'>英玲老師</font></a></td>");
 }	
 document.write("      </tr>");
 //document.write("      <tr>");
@@ -43,9 +43,17 @@ if (varleft!=5) {
 document.write("      </tr>");
 document.write("      <tr>");
 if (varleft!=6) {
-	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher6.htm'>淑華老師</a></td>");
+	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher6.htm'>Debby老師</a></td>");
 } else {
-	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher6.htm'><font color='#ffffff'>淑華老師</font></a></td>");
+	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher6.htm'><font color='#ffffff'>Debby老師</font></a></td>");
 }	
 document.write("      </tr>");
+document.write("      <tr>");
+if (varleft!=7) {
+	document.write("        <td align='center' bgcolor='#92AD6F' bordercolor='#90AA6D'><a href='teacher7.htm'>芬芬老師</a></td>");
+} else {
+	document.write("        <td align='center' bgcolor='#869E5C' bordercolor='#90AA6D'><a href='teacher7.htm'><font color='#ffffff'>王芬芬老師</font></a></td>");
+}	
+document.write("      </tr>");
+
 document.write("</table>");
