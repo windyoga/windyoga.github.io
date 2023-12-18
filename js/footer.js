5b60872676888052a9926b85cb64a767b7398b46
cowcowlin2 <cowcowlin2@gmail.com>
2023-12-18 14:29:49 +0800

    20231218_V1
    20231218

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+document.write("<table class='contentshow' border='0' cellpadding='0' cellspacing='0' bgcolor='#FFFFFF'> ");
+document.write("  <tr> ");
+document.write("    <td height='26' align='center' valign='top' background='img/footer.gif'><p class='footer'>華云地址 : 臺北市士林區中正路327號3F 聯絡電話 : 02-2882-9678 </p></td> ");
+document.write("  </tr> ");
+document.write("</table> ");
---
+++ js/trx_leftmenu.js
@@ -0,0 +1,15 @@
+document.write("	<div class='col-lg-2 hidden-sm hidden-xs' style='background:url(img/bg7a.jpg);' >	");
+document.write("		<ul class='list-group leftmenuword'>	");
+document.write("		  <li class='list-group-item list-group-item-info " + (varleft==0?"active":"") + "'><a href='trx_teacher2.htm'>Leo教練</a></li>	");
+document.write("		  <li class='list-group-item list-group-item-info " + (varleft==1?"active":"") + "'><a href='trx_teacher.htm'>光光教練</a></li>	");
+document.write("		</ul>				");
+document.write("	</div>	");
+document.write("	<div class='dropdown hidden-lg hidden-md col-sm-12 col-xs-12'style='padding:0px;'>	");
+document.write("		<button class='btn btn-danger dropdown-toggle' type='button' data-toggle='dropdown' style='background:#7c3477;width:100%'>教練	");
+document.write("			<span class='caret'></span>	");
+document.write("		</button>	");
+document.write("		<ul class='dropdown-menu' style='width:100%'>	");
+document.write("			<li><a href='trx_teacher2.htm'>Leo教練</a></li>	");
+document.write("			<li><a href='trx_teacher.htm'>光光教練</a></li>	");
+document.write("		</ul>");
+document.write("	 </div>		");
